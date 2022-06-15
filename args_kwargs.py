db8e1986fd942f3184485cbf901fc45039877eaf
DESKTOP-REGETCI\SinYoung <uky00832@gmail.com>
2022-06-15 16:45:02 +0900

    Revert "args, kwargs 예제 코드"
    This reverts commit 1c777b32a55ae76c757f4b4129c0a32d7eac44f3.

--- args_kwargs.py
+++
@@ -1,7 +0,0 @@
-list_ = ['one', 'two', 'three', 'four', 'five']
-dic_ = {1:'one', 2:'two', 3:'three', 4:'four', 5:'five'}
-
-def test(a, b, *args, **kwargs):
-    print(a, b, *args, **kwargs)
-
-test(1, 2, list_, dic_)
