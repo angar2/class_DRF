f0e041551224b2ea8553919f05d94fa69a857926
DESKTOP-REGETCI\SinYoung <uky00832@gmail.com>
2022-06-19 18:49:32 +0900

    Feat: blog Comment Model 생성

--- blog/models.py
+++ blog/models.py
@@ -11,3 +11,9 @@ class Article(models.Model):
     title = models.CharField(("글 제목"), max_length=200)
     body = models.TextField(("글 내용"), max_length=1000)
     category = models.ManyToManyField('Category')
+
+
+class Comment(models.Model):
+    article = models.ForeignKey('Article', verbose_name="게시글", on_delete=models.CASCADE)
+    user = models.ForeignKey('user.User', verbose_name="작성자", on_delete=models.CASCADE)
+    content = models.CharField(("댓글"), max_length=200)
