2f9629e4b2bc1b7146c685a68485c3e60006a488
DESKTOP-REGETCI\SinYoung <uky00832@gmail.com>
2022-06-19 20:08:14 +0900

    Feat: user 정보 조회 시 article, comment 정보 추가로 출력

--- user/serializers.py
+++ user/serializers.py
@@ -1,8 +1,37 @@
+from unicodedata import category
 from rest_framework import serializers
 
 from user.models import User as UserModel
 from user.models import UserProfile as UserProfileModel
 from user.models import Hobby as HobbyModel
+from blog.models import Article as ArticleModel
+from blog.models import Category as CategoryModel
+from blog.models import Comment as CommentModel
+
+class CommentSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = CommentModel
+        fields = ["author", "content"]
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    
+    class Meta:
+            model = CategoryModel
+            fields = ["name"]
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+
+    comments = CommentSerializer(many=True, source="comment_set")
+    category = CategorySerializer(many=True)
+
+    class Meta:
+        model = ArticleModel
+        fields = ["author", "title", "body", "category", "comments"]
+        # fields = ["author", "title", "body", "category", "comment"]
+
 
 class HobbySerializer(serializers.ModelSerializer):
 
@@ -40,7 +69,8 @@ class UserSerializer(serializers.ModelSerializer):
     # 다른 serializer class를 가져다 사용할 수 있음
     # (sourec="userprofile")로 filed명도 변경 가능함
     userprofile = UserProfileSerializer()
+    article = ArticleSerializer(many=True, source="article_set")
 
     class Meta:
         model = UserModel
-        fields = ["username", "email", "fullname", "join_date", "userprofile"]
+        fields = ["username", "email", "fullname", "join_date", "userprofile", "article"]
