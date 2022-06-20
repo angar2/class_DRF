a5b4ece9deb15d1780ed09766b6403334e0c5a70
DESKTOP-REGETCI\SinYoung <uky00832@gmail.com>
2022-06-20 15:24:14 +0900

    Feat: UserAdmin을 이용한 admin custom

--- user/admin.py
+++ user/admin.py
@@ -2,9 +2,29 @@ from django.contrib import admin
 from user.models import User as UserModel
 from user.models import UserProfile as UserProfileModel
 from user.models import Hobby as HobbyModel
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 
-# Register your models here.
-admin.site.register(UserModel)
+# admin으로 user 정보를 저장할 때, 비밀번호가 hashing되지 않기 때문에 Django에서 제공하는 UserAdmin을 상속받아 사용함
+class UserAdmin(BaseUserAdmin):
+    list_display = ('id', 'username', 'fullname', 'email')
+    list_display_links = ('username', )
+    list_filter = ('username', )
+    search_fields = ('username', 'email', )
+
+    fieldsets = (
+        ("info", {'fields': ('username', 'password', 'email', 'fullname', 'join_date',)}),
+        ('Permissions', {'fields': ('is_admin', 'is_active', )}),)
+
+    filter_horizontal = []
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return ('username', 'join_date', )
+        else:
+            return ('join_date', )
+
+
+admin.site.register(UserModel, UserAdmin)
 admin.site.register(UserProfileModel)
 admin.site.register(HobbyModel)
